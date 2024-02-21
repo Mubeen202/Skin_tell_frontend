aa13df154ecded2d6b52caa3463e9d1fc3f55549
Mubeen202 <80257786+Mubeen202@users.noreply.github.com>
2024-02-21 16:44:51 +0400

    happy flow

--- SkinTell/App.js
+++ SkinTell/App.js
@@ -3,17 +3,50 @@ import { StyleSheet, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SignInNavigation, SignOutNavigation } from './Apps/Navigations/Navigations';
+import { ClerkProvider,  SignedIn, SignedOut  } from '@clerk/clerk-expo';
+import Configrations from './Utils/Configrations.js/Configrations';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import * as SecureStore from "expo-secure-store";
+ 
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  
   return (
+    <ClerkProvider publishableKey={Configrations.SERVICE_API_KEY} tokenCache={tokenCache}>
+      <SafeAreaView style={styles.container}>
     <NavigationContainer>
+    <SignedIn>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="SignIn" component={SignInNavigation} />
+
+    </Stack.Navigator>
+      </SignedIn>
+      <SignedOut>
       <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="SignIn" component={SignInNavigation} /> */}
         <Stack.Screen name="SignOut" component={SignOutNavigation} />
       </Stack.Navigator>
+      </SignedOut>
     </NavigationContainer>
+    </SafeAreaView>
+    </ClerkProvider>
   );
 }
 
--- SkinTell/Apps/Navigations/Navigations.jsx
+++ SkinTell/Apps/Navigations/Navigations.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import DashboardScreen from '../Dashboard/DashboardScreen';
 import StartUp from '../Startup/StartUp';
 import Loby from '../Loby/Loby';
 import RegisterScreen from '../Loby/SingUp/RegisterScreen';
 import LoginScreen from '../Loby/Login/LoginScreen';
+import HomeScreen from '../Dashboard/HomeScreen';
 const Stack = createStackNavigator();
 
 export const SignInNavigation = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
-   <Stack.Screen name="Dashboard" component={DashboardScreen} />
+   <Stack.Screen name="HomeScreen" component={HomeScreen} />
   </Stack.Navigator>
 );
 
@@ -20,7 +20,6 @@ export const SignOutNavigation = () => (
       <Stack.Screen name="Loby" component={Loby} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Dashboard" component={DashboardScreen} />
   </Stack.Navigator>
 );
 
